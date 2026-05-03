7aa5c56cbf21c74fd5066984dbfe2e4401f5d39c
Frixuu <code@frixuu.fyi>
2026-05-03 02:38:34 +0200

    feat: fetch new license exceptions

--- scripts/FetchLicenses.hx
+++ scripts/FetchLicenses.hx
@@ -7,31 +7,34 @@ import sys.FileSystem;
 import sys.Http;
 import sys.io.File;
 
-final class FetchLicenses {
+function saveJsonToDataFile(url: String, fileName: String): Void {
 
-    static final URL = "https://raw.githubusercontent.com/spdx/license-list-data/refs/heads/main/json/licenses.json";
-
-    private static function main(): Void {
-
-        trace("Fetching data from " + URL + "...");
-
-        var ok = false;
-        var json = "";
-        while (!ok) {
-            json = Http.requestUrl(URL);
-            trace("Downloaded data. Length: " + json.length);
-            try {
-                Json.parse(json);
-                ok = true;
-            } catch (e: Any) {
-                trace("Failed to parse JSON. Retrying...");
-                Sys.sleep(0.5);
-            }
+    trace('Fetching data from $url...');
+    
+    var ok = false;
+    var json = "";
+    while (!ok) {
+        json = Http.requestUrl(url);
+        trace('Downloaded data. (length: ${json.length})');
+        try {
+            Json.parse(json);
+            ok = true;
+        } catch (_e: Any) {
+            trace("Data downloaded, but seems to be corrupted. Retrying...");
+            Sys.sleep(0.5);
         }
-
-        trace("Saving data...");
-        final dir = Path.join([Sys.getCwd(), "data"]);
-        FileSystem.createDirectory(dir);
-        File.saveContent(Path.join([dir, "licenses.json"]), json);
     }
+    
+    trace('Saving data to $fileName...');
+    final dir = Path.join([Sys.getCwd(), "data"]);
+    FileSystem.createDirectory(dir);
+    File.saveContent(Path.join([dir, fileName]), json);
+}
+
+function main(): Void {
+
+    final repo = "https://raw.githubusercontent.com/spdx/license-list-data";
+    final ref = "refs/heads/main";
+    saveJsonToDataFile('$repo/$ref/json/licenses.json', "licenses.json");
+    saveJsonToDataFile('$repo/$ref/json/exceptions.json', "exceptions.json");
 }
